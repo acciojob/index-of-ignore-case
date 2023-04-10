46340f5b70006dcaa9f2a70afdce9a16a8d4bbb3
aj2000 <hi@ajaysharma.dev>
2023-04-10 16:34:06 +0530

    read me update

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+function indexOfIgnoreCase(s1, s2) {
+  // write your code here
+}
+
+// Please do not change the code below
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
